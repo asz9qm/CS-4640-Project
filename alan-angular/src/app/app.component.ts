be038a6d8073dab26e2982d3140d75284bb058d7
asz9qm <asz9qm@virginia.edu>
2020-04-27 02:17:27 -0400

    assignment 4 end

--- alan-angular/src/app/app.component.ts
+++ alan-angular/src/app/app.component.ts
@@ -13,7 +13,6 @@ export class AppComponent {
   title = 'UVA Semester Scheduler';
   data_submitted = "";
   confirm_msg = "";
-  user = "";
   done = false; // checks if course has been added
   
   //-----------------Form answer setups-----------------//
@@ -28,35 +27,36 @@ export class AppComponent {
     "A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F", "CR", "NC"
   ]
   // creating course object to bind to and from
-  responsedata = new Course("", "", "", true, "", "");
-  courseModel = new Course("General Requirement", "Course Mnemonic", "Course Name", true, "", "");
-  constructor(private http: HttpClient){ }
+  responsedata = new Course("", "", "", true, "", "", "");
+  courseModel = new Course("General Requirement", "Course Mnemonic", "Course Name", true, "", "", "");
 
-  getUser(): Observable<any>{
-    return this.http.get('http://localhost/CS-4640-Project/PHP/getUser.php');
+  constructor(private http: HttpClient){ 
+    this.setUser2(this.courseModel);
+  }
+
+  setUser2(course: Course): void{
+    this.http.get('http://localhost/CS-4640-Project/PHP/getUser.php',{withCredentials:true})
+    .subscribe(
+    (data)=>{
+      course["email"]=(data["user"]);
+    });
+
   }
 
   confirmSubmit(course: Course): void{
+    
     // submits and send post information to post file
     this.confirm_msg += course.courseID + " " + course.courseName + " was added";
     this.done = true;
 
-    console.log("Hello");
-    console.log(sessionStorage.getItem("user"));
-    console.log(sessionStorage);
-    console.log("Hello");
-
-    // this.getUser().subscribe(
-    //   (data)=>{
-    //     this.user = data[0].user;
-    //     console.log(this.user);
-    //   });
+    // console.log(course);
     this.sendPost(course).subscribe(
       res=>{
         console.log(res);
       }
     )
-    setTimeout(function(){ window.location.href = "http://localhost/CS-4640-Project/PHP/requirementsPage.php"; }, 2000);
+
+    // setTimeout(function(){ window.location.href = "http://localhost/CS-4640-Project/PHP/requirementsPage.php"; }, 2000);
     
   }
 
